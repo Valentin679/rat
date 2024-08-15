f1d9a27e94ddf0129819b606bd2d225e959a7d39
Valentin679 <67257342+Valentin679@users.noreply.github.com>
2024-08-15 21:12:08 +0300

    headerUI

--- src/app/components/modules/Header/Header.js
+++ src/app/components/modules/Header/Header.js
@@ -1,49 +1,63 @@
 import styles from "@/app/components/modules/Header/header.module.css";
 import {CgProfile} from "react-icons/cg";
 import {FiShoppingBag} from "react-icons/fi";
-import { FaRegHeart } from "react-icons/fa";
+import {FaRegHeart} from "react-icons/fa";
+import { SlSocialVkontakte } from "react-icons/sl";
+import { LiaTelegram } from "react-icons/lia";
 import localFont from "next/font/local";
 import Logo from "@/app/components/modules/Logo/Logo";
 
 const myFont = localFont({src: '../../../DrukWideCyrBold.woff'})
+const golosBold = localFont({src: '../../../fonts/GolosText-Bold.ttf'})
+const golosMedium = localFont({src: '../../../fonts/GolosText-Medium.ttf'})
 
 export default function Header({title}) {
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
-                <ul className={styles.navLeft}>
-                    <li>ДОСТАВКА И ОПЛАТА</li>
-                    <li>КОНТАКТЫ</li>
-                </ul>
-                <ul className={styles.navRight}>
-                    <li>МАГАЗИН</li>
-                    <li>КОНСТРУКТОР</li>
-                    <li>ПОДАРКИ</li>
-                </ul>
-                <ul className={styles.navSocial}>
-                    <li>8 (900) 228-67-15</li>
-                </ul>
+                <div className={styles.container}>
+                    <ul className={styles.navLeft}>
+                        <li className={golosMedium.className}>ДОСТАВКА И ОПЛАТА</li>
+                        <li>КОНТАКТЫ</li>
+                    </ul>
+                    <ul className={styles.navRight}>
+                        <li>МАГАЗИН</li>
+                        <li>КОНСТРУКТОР</li>
+                        <li>ПОДАРКИ</li>
+                    </ul>
+                    <ul className={styles.navSocial}>
+                        <li><SlSocialVkontakte
+                            size={25}
+                        /></li>
+                        <li><LiaTelegram
+                            size={25}
+                        /></li>
+                        <li>8 (900) 228-67-15</li>
+                    </ul>
+                </div>
             </nav>
             <div className={styles.navMiddle}>
-                <Logo/>
-                <div className={styles.socialIcons}>
-                    <div className={styles.cartIcon}>
-                        <CgProfile
-                            size={40}
-                        />
-                        <p className={myFont.className}>Профиль</p>
-                    </div>
-                    <div className={styles.cartIcon}>
-                        <FaRegHeart
-                            size={40}
-                        />
-                        <p className={myFont.className}>Избранное</p>
-                    </div>
-                    <div className={styles.cartIcon}>
-                        <FiShoppingBag
-                            size={40}
-                        />
-                        <p className={myFont.className}>Корзина</p>
+                <div className={styles.container}>
+                    <Logo/>
+                    <div className={styles.socialIcons}>
+                        <div className={styles.cartIcon}>
+                            <CgProfile
+                                size={25}
+                            />
+                            <p className={golosBold.className}>Профиль</p>
+                        </div>
+                        <div className={styles.cartIcon}>
+                            <FaRegHeart
+                                size={25}
+                            />
+                            <p className={golosBold.className}>Избранное</p>
+                        </div>
+                        <div className={styles.cartIcon}>
+                            <FiShoppingBag
+                                size={25}
+                            />
+                            <p className={golosBold.className}>Корзина</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/app/components/modules/NavHoliday/NavHoliday.js
+++ src/app/components/modules/NavHoliday/NavHoliday.js
@@ -2,17 +2,17 @@ import localFont from 'next/font/local';
 import styles from "@/app/components/modules/NavHoliday/navholiday.module.css";
 
 const myFont = localFont({ src: '../../../DrukWideCyrBold.woff' })
-
+const golosBold = localFont({src: '../../../fonts/GolosText-Bold.ttf'})
 
 export default function NavHoliday({ children }) {
     return (
         <div className={styles.navHoliday}>
             <div className='containerRow'>
-            <div className={myFont.className}>1 сентября</div>
-            <div className={myFont.className}>День учителя</div>
-            <div className={myFont.className}>Новый год</div>
-            <div className={myFont.className}>8 Марта</div>
-            <div className={myFont.className}>23 Февраля</div>
+            <div className={golosBold.className}>1 сентября</div>
+            <div className={golosBold.className}>День учителя</div>
+            <div className={golosBold.className}>Новый год</div>
+            <div className={golosBold.className}>8 Марта</div>
+            <div className={golosBold.className}>23 Февраля</div>
             </div>
         </div>
     );
