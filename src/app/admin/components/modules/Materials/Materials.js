14ed3241badfa4f7920960b0b9169e84927a27ce
Rat <mezeninvalentin@yandex.ru>
2024-08-22 21:11:07 +0300

    materialEnd1

--- src/app/admin/components/modules/Materials/Materials.js
+++ src/app/admin/components/modules/Materials/Materials.js
@@ -2,9 +2,9 @@ import styles from "@/app/admin/components/modules/Materials/materials.module.cs
 import React, {useEffect, useRef, useState} from "react";
 import AddMaterials from "@/app/admin/components/modules/Materials/AddMaterials";
 import {deleteCategory, GetCategories, putCategory} from "@/app/api/fetchCategories";
-import { FaEdit } from "react-icons/fa";
-import { RiDeleteBin6Line } from "react-icons/ri";
-import { RiDeleteBin2Line } from "react-icons/ri";
+import {FaEdit} from "react-icons/fa";
+import {RiDeleteBin6Line} from "react-icons/ri";
+import {RiDeleteBin2Line} from "react-icons/ri";
 
 export default function Materials({categories}) {
     const [catList, setCatList] = useState([])
@@ -53,28 +53,36 @@ export default function Materials({categories}) {
             <div className={styles.itemList}>
                 {catList.map(e => changedStateId !== e.title ?
                     <div key={e.slug} className={styles.item}>
-                        <div onDoubleClick={() => {
-                            setChangedStateId(e.title)
-                            setOldSlug(e.slug)
-                            setTitle(e.title)
-                            setSlug(e.slug)
-                        }} >{e.title}
+                        <div >{e.title}
                         </div>
-                        <div className={styles.itemDelete}>
-                            {/*<FaEdit*/}
-                            {/*    size={25}*/}
-                            {/*/>*/}
-                            <RiDeleteBin2Line onClick={()=>{onDelete(e.slug)}}
-                                size={22}
-                            />
+                        <div className={styles.itemEditDel}>
+                            <div className={styles.itemEdit}>
+                                <FaEdit onClick={() => {
+                                    setChangedStateId(e.title)
+                                    setOldSlug(e.slug)
+                                    setTitle(e.title)
+                                    setSlug(e.slug)
+                                }}
+                                    size={23}
+                                />
+                            </div>
+                            <div className={styles.itemDelete}>
+                                <RiDeleteBin2Line onClick={() => {
+                                    onDelete(e.slug)
+                                }}
+                                                  size={23}
+                                />
+                            </div>
                         </div>
                     </div>
                     :
-                    <div key={e.slug}>
+                    <>
+                    <div key={e.slug} className={styles.addInputBox}>
                         <input id="title" onChange={onChange} type="text" name="title" value={title}/>
                         <input id="slug" onChange={onChange} type="text" name="slug" value={slug}/>
-                        <button onClick={onSave}>SAVE</button>
                     </div>
+                        <button onClick={onSave}>Обновить</button>
+                    </>
                 )}
             </div>
             <div className={styles.addItemList}>
